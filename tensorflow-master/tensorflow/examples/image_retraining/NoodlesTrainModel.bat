ab60b817f2ed1829a7e613e7a853627cc5c1150a
ting.wong <ting.wong@tpv-tech.com>
2018-04-09 14:04:26 +0800

    1
    1

--- tensorflow-master/tensorflow/examples/image_retraining/NoodlesTrainModel.bat
+++ tensorflow-master/tensorflow/examples/image_retraining/NoodlesTrainModel.bat
@@ -9,6 +9,7 @@ if /i %HH% LSS 10 (set HH=0%time:~1,1%)
 set DateTime=%date:~0,4%%date:~5,2%%date:~8,2%_%HH%%time:~3,2%%time:~6,2%
 cd %dirwork%
 E:
-set diroutput=noodles/output/%DateTime%_noodles_
-python retrain.py --image_dir noodles/input/ --how_many_training_steps=50 --architecture mobilenet_1.0_224 --output_labels %diroutput%labels.txt --output_graph %diroutput%graph.pb
+set diroutput=tpv/output/%DateTime%_tpv_
+echo --final_tensor_name=output 
+python retrain.py --image_dir tpv/input/ --how_many_training_steps=1000 --architecture mobilenet_1.0_224 --output_labels %diroutput%labels.txt --output_graph %diroutput%graph.pb
 pause
