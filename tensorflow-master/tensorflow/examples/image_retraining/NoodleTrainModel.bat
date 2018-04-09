4f33e334f946f1fc65f8fbebfe903bce85b5c4c9
ting.wong <ting.wong@tpv-tech.com>
2018-04-09 10:30:55 +0800

    demo
    demo

--- tensorflow-master/tensorflow/examples/image_retraining/NoodleTrainModel.bat
+++
@@ -1,7 +0,0 @@
-echo 运行retrain.py脚本
-echo image_dir 是图片文件夹名  
-echo architecture 是所选用的模型  
-echo output_labels 是输出的模型名  
-echo output_graph 是模型对应的label文件     
-python retrain.py --image_dir noodles --architecture mobilenet_1.0_224 --output_labels noodles_labels.txt --output_graph noodles_graph.pb
-pause
---
+++ tensorflow-master/tensorflow/examples/image_retraining/NoodlesTrainModel.bat
@@ -0,0 +1,14 @@
+﻿echo 运行retrain.py脚本
+echo image_dir 是图片文件夹名  
+echo architecture 是所选用的模型  
+echo output_labels 是输出的模型名  
+echo output_graph 是模型对应的label文件 
+set dirwork=E:\GitHub\Android_MobileNet_TensorFlow\tensorflow-master\tensorflow\examples\image_retraining
+set HH=%time:~0,2%
+if /i %HH% LSS 10 (set HH=0%time:~1,1%)
+set DateTime=%date:~0,4%%date:~5,2%%date:~8,2%_%HH%%time:~3,2%%time:~6,2%
+cd %dirwork%
+E:
+set diroutput=noodles/output/%DateTime%_noodles_
+python retrain.py --image_dir noodles/input/ --how_many_training_steps=50 --architecture mobilenet_1.0_224 --output_labels %diroutput%labels.txt --output_graph %diroutput%graph.pb
+pause
