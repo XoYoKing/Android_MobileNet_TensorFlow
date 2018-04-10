1ef4823ead9a42f1210a4af6fbe68539675419fd
ting.wong <ting.wong@tpv-tech.com>
2018-04-10 11:36:22 +0800

    1
    1

---
+++ tensorflow-master/tensorflow/examples/image_retraining/TpvTest.bat
@@ -0,0 +1,9 @@
+﻿set dirwork=E:\TpvTrain
+set test=E:\GitHub\Android_MobileNet_TensorFlow\tensorflow-master\tensorflow\examples\label_image\label_image.py
+cd %dirwork%
+E:
+set dirgraph=output/tpv_graph.pb
+set dirlabels=output/tpv_labels.txt
+set dirimg=input\noodles\1.jpg
+python %test% --graph=%dirgraph% --labels=%dirlabels% --image=%dirimg% --input_layer=input --output_layer=final_result --input_mean=128 --input_std=128 --input_width=224 --input_height=224
+pause
---
+++ tensorflow-master/tensorflow/examples/image_retraining/TpvTrain.bat
@@ -0,0 +1,17 @@
+﻿echo 运行retrain.py脚本
+echo image_dir 是图片文件夹名  
+echo architecture 是所选用的模型  
+echo output_labels 是输出的模型名  
+echo output_graph 是模型对应的label文件 
+set dirwork=E:\TpvTrain
+set retrain=E:\GitHub\Android_MobileNet_TensorFlow\tensorflow-master\tensorflow\examples\image_retraining\retrain.py
+set HH=%time:~0,2%
+if /i %HH% LSS 10 (set HH=0%time:~1,1%)
+set DateTime=%date:~0,4%%date:~5,2%%date:~8,2%_%HH%%time:~3,2%%time:~6,2%
+cd %dirwork%
+E:
+echo set diroutput=output/%DateTime%_tpv_
+set diroutput=output/tpv_
+echo --final_tensor_name=output 
+python %retrain% --image_dir input/ --how_many_training_steps=1000 --architecture mobilenet_1.0_224 --learning_rate=0.0001 --testing_percentage=20 --validation_percentage=20 --train_batch_size=32 --validation_batch_size=-1 --output_labels %diroutput%labels.txt --output_graph %diroutput%graph.pb
+pause
